85305b2098753412c18a7ff5c22fc7bbd923838f
TullioTambone <tulliotambone97@gmail.com>
2023-05-15 16:52:37 +0200

    creazione card eventi inizio creazione 2° carosello

---
+++ src/components/CarouselWrapperComp6thSection.vue
@@ -0,0 +1,157 @@
+<script>
+import { defineComponent } from 'vue'
+import { Carousel, Pagination, Slide, Navigation } from 'vue3-carousel'
+
+import 'vue3-carousel/dist/carousel.css'
+
+export default defineComponent({
+    name: 'Autoplay',
+    components: {
+        Carousel,
+        Slide,
+        Pagination,
+        Navigation
+    },
+    data: () => ({
+
+        cards:[
+            {
+                title:'High level of efficiency and scientific teaching methods',
+                commento:'i am free to learn lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque hic culpa totam, voluptatem necessitatibus eius veniam officiis',
+                img:'/images/',
+                nome:'MINA HOLLACE',
+                job:'Freelancer',
+            },
+            {
+                title:'The MaxCoach team work really hard to ensure high quality',
+                commento:'i am free to learn lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque hic culpa totam, voluptatem necessitatibus eius veniam officiis',
+                img:'/images/',
+                nome:'LUVIC DUBBLE',
+                job:'Private Tutor',
+            },
+            {
+                title:"it's a choice of quality for people with special needs",
+                commento:'i am free to learn lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque hic culpa totam, voluptatem necessitatibus eius veniam officiis',
+                img:'/images/',
+                nome:'FLORANCE THEMES',
+                job:'Multimedia Admin',
+            },
+            {
+                title:'High level of efficiency and scentific teaching methods',
+                commento:'i am free to learn lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque hic culpa totam, voluptatem necessitatibus eius veniam officiis',
+                img:'/images/',
+                nome:'MINA HOLLACE',
+                job:'Freelancer',
+            },
+        ],
+        // carousel settings
+        settings: {
+        itemsToShow: 1,
+        snapAlign: 'center',
+        },
+        // breakpoints are mobile first
+        // any settings not specified will fallback to the carousel settings
+        breakpoints: {
+
+            300:{
+                itemsToShow: 2,
+                snapAlign: 'center',
+            },
+            // 700px and up
+            700: {
+                itemsToShow: 2,
+                snapAlign: 'center',
+            },
+            // 1024 and up
+            1024: {
+                itemsToShow: 2.3,
+                snapAlign: 'center',
+            },
+        },
+    }),
+})
+</script>
+<template>
+    <Carousel :itemsToShow="3.3" :wrapAround="true" :transition="500" :autoplay="4000" v-bind="settings" :breakpoints="breakpoints">
+        <Slide v-for="(element, index) in this.cards" :key="index" class="mb-5">
+            <div class="carousel__item p-1">
+                <h6 id="title">{{ element.title }}</h6>
+                <p id="commento">
+                    {{ element.commento }}
+                </p>
+            </div>
+        </Slide>
+        <template #addons class="m-auto">
+            <Pagination />
+        </template>
+    </Carousel>
+</template>
+
+<style lang="scss" scoped>
+@import '../style/main.scss';
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+#title{
+    color: $tuna;
+    font-weight: 600;
+
+}
+
+#commento{
+    font-size: medium;
+    color:$boulder;
+}
+.carousel__item{
+    background-color: white;
+}
+
+.carousel__pagination{
+    padding-top: 1.5rem;
+    background-color: none;
+}
+
+.carousel__slide {
+padding: 5px;
+}
+
+.carousel__viewport {
+    perspective: 2000px;
+    position: relative;
+    z-index: 99;
+}
+
+.carousel__track {
+transform-style: preserve-3d;
+}
+
+.carousel__slide--sliding {
+transition: 0.5s;
+}
+
+.carousel__slide {
+opacity: 0.9;
+transform: rotateY(-20deg) scale(0.9);
+}
+
+.carousel__slide--active ~ .carousel__slide {
+transform: rotateY(20deg) scale(0.9);
+}
+
+.carousel__slide--prev {
+opacity: 1;
+transform: rotateY(-10deg) scale(0.95);
+}
+
+.carousel__slide--next {
+opacity: 1;
+transform: rotateY(10deg) scale(0.95);
+}
+
+.carousel__slide--active {
+opacity: 1;
+transform: rotateY(0) scale(1.1);
+}
+</style>
--- src/components/SixthMainSection.vue
+++ src/components/SixthMainSection.vue
@@ -1,30 +1,46 @@
 <script>
+import Autoplay from './CarouselWrapperComp6thSection.vue'
  export default{
     name:'SixthMAinSection',
+    components:{
+        Autoplay
+    },
     data(){
         return{
             arrayEvents: [
                 {
+                    luogo:'Texas, US',
+                    event:'Storytelling Workshop',
                     number: '22',
                     mese: 'NOV',
                 },
                 {
+                    luogo:'New York, US',
+                    event:'Painting Art Contest 2020',
                     number: '10',
                     mese: 'OCT',
                 },
                 {
+                    luogo:'Hamburg, Germany',
+                    event:'International Art Fair 2020',
                     number: '23',
                     mese: 'NOV',
                 },
                 {
+                    luogo:'Illinois, US',
+                    event:'Street Performance: Call for Artist',
                     number: '15',
                     mese: 'DEC',
                 },
                 {
+                    luogo:'Illinois, US',
+                    event:'Consumer Food Safety Education Conference',
                     number: '22',
                     mese: 'JUL',
                 },
                 {
+                    luogo:'Dubai',
+                    event:'How meditation improve your mental health?',
                     number: '12',
                     mese: 'AUG',
                 },
@@ -43,12 +59,15 @@
         </div>
 
         <div class="row mt-5">
-            <div class="my-card col-12 col-lg-6 d-flex justify-content-between" v-for="(element, index) in arrayEvents">
-                <div>
-                    <span>{{  }}</span>
-                    <h5 class="mt-2"></h5>
+            <div class="my-card d-flex justify-content-between" v-for="(element, index) in arrayEvents">
+                <div class="w-75">
+                    <span class="luogo">
+                        <i class="fa-solid fa-location-dot"></i>
+                        {{ element.luogo }}
+                    </span>
+                    <h5 class="mt-2 event">{{ element.event }}</h5>
                 </div>
-                <div class="d-flex flex-column align-items-center">
+                <div class="d-flex flex-column align-items-center ms-1">
                     <span class="numbers">{{ element.number }}</span>
                     <span class="mese">{{ element.mese }}</span>
                     <button>Get ticket</button>
@@ -65,6 +84,10 @@
             <h6>People are praising Maxcoach</h6>
             <h2>What make they love us?</h2>
         </div>
+
+        <div class="container-fluid">
+            <Autoplay/>
+        </div>
     </div>
 </section>
 </template>
@@ -77,8 +100,7 @@
     padding: 0;
     .border-container{
         background-color: white;
-        height: 80vh;
-        border-radius: 0 0 50% 0;
+        border-radius: 0 0 110em/12em 0;
         .static-text{
             h6{
                 color: $giungla;
@@ -89,15 +111,86 @@
             }
         }
         .row{
+            gap: 20px;
+            padding-bottom: 10rem;
+            padding-left: 2rem;
+            padding-right: 2rem;
             .my-card{
+                padding: 15px;      
+                border-radius: 5px;
                 background-color: $mywhite;
+                flex-basis: calc(100% / 2 - 20px);
+
+                div{
+                    .event{
+                        font-weight: 600;
+                        font-size: medium;
+                    }
+                    .luogo{
+                        color: $boulder;
+                    }
+                    .numbers{
+                        color: $giungla;
+                        font-size: 30px;
+                        font-weight: 500;
+                    }
+
+                    .mese{
+                        font-weight: 600;
+                        font-size: 13px;
+                    }
+                    button{
+                        padding: 10px 25px;
+                        font-size: 12px;
+                        border:none;
+                        border-radius: 10px;
+                        background-color: $giungla;
+                        color: white;
+                    }
+                }
+
+                &:hover{
+                    border-left: 3px solid $giungla;
+                    background-color: $white;
+                    box-shadow: 0 0 40px lightgray;
+                    transition: 0.1s;
+                }
+
+                &:hover button{
+                    color: white;
+                    background-color: $purple;
+                    transition: 0.4s;
+                }
+            }
+        }
+    }
+}
+
+@media screen and (max-width: 767px) {
+    .first-section{
+        .border-container{
+            .row{
+                .my-card{
+                    flex-basis: 100%
+                }
             }
         }
     }
 }
 
 .second-section{
-    height: 20vh;
     background-color: $mywhite;
+
+    .border-container{
+        .static-text{
+                h6{
+                    color: $giungla;
+                    font-weight: 600;
+                }
+                h2{
+                    font-weight: 700;
+                }
+            }
+    }
 }
 </style>
